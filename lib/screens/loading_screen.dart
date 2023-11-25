151a3e7da9aa2caf9a99ccfc39303377351f9fec
somelelequse <QSXSOM001@myuct.ac.za>
2023-11-25 06:00:36 +0200

    I should move on

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -18,40 +18,42 @@ class _LoadingScreenState extends State<LoadingScreen> {
         backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Chaos.',
               style: TextStyle(
                   fontSize: 70,
                   fontWeight: FontWeight.w900,
                   letterSpacing: -10,
                   height: 1,
-                  color: Colors.black),
+                  color: Color(0xff010b13)),
             ),
-            SizedBox(
+            const SizedBox(
               height: 120,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'Let\'s get productive!',
-                  style: TextStyle(fontWeight: FontWeight.w400),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                CircleAvatar(
-                  backgroundColor: Colors.black,
-                  radius: 22,
-                  child: Icon(
-                    CupertinoIcons.arrow_right,
-                    color: Colors.white,
+                TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor: const MaterialStatePropertyAll<Color>(
+                          Color(0xff010b13)),
+                      shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)))),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Get started now',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w400),
+                    ),
                   ),
                 )
               ],
@@ -79,4 +81,4 @@ class _LoadingScreenState extends State<LoadingScreen> {
 //                     //         borderRadius: BorderRadius.circular(10)))
 //                   ),
 //                   child: const Icon(CupertinoIcons.arrow_right),
-//                 )
+//                 ))
