8303f9c4b51005b7b00c826d6b81d94ac2c9917e
somelelequse <QSXSOM001@myuct.ac.za>
2023-11-25 04:57:37 +0200

    keep it simple.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -12,8 +12,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blueGrey,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Chaos',
+              style: TextStyle(
+                  fontSize: 70,
+                  fontWeight: FontWeight.w700,
+                  height: 1,
+                  color: Colors.black),
+            ),
+            SizedBox(
+              height: 40,
+            )
+          ],
+        ),
       ),
     );
   }
