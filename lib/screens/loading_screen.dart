9dcffdb54950a84b11713df515a924efda47c68c
somelelequse <QSXSOM001@myuct.ac.za>
2023-11-25 05:26:36 +0200

    I think I like this one.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -17,22 +17,45 @@ class _LoadingScreenState extends State<LoadingScreen> {
         backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'Chaos',
+            const Text(
+              'Chaos.',
               style: TextStyle(
                   fontSize: 70,
-                  fontWeight: FontWeight.w700,
+                  fontWeight: FontWeight.w900,
+                  letterSpacing: -10,
                   height: 1,
                   color: Colors.black),
             ),
-            SizedBox(
-              height: 40,
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor:
+                          const MaterialStatePropertyAll<Color>(Colors.black),
+                      shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)))),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Get started now',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ],
             )
           ],
         ),
